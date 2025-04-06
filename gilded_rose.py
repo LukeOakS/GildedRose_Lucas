44b9f2842bdfe7c8eef284bf4b5005e8db72d6a4
LukeOakS <lucas.c.silva99@outlook.com>
2025-04-06 19:09:10 -0300

    Ajustes finais na lógica das funções geral e privadas

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,29 +8,34 @@ class GildedRose(object):
     def update_quality(self):
         for item in self.items:
 
-            if item.quality < 50:
+            if (item.quality > 0 and item.quality < 50) or item.name == "Aged Brie":
+
                 if item.name == "Aged Brie":
                     self.__handle_aged_brie(item)
 
                 elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                     self.__handle_backstage(item)
-            
-                elif item.name == "Sulfuras, Hand of Ragnaros":
-                    self.__handle_sulfuras(item)
                 
                 elif item.name == "Conjured Mana Cake":
-                    if item.quality > 0:
-                        self.__handle_conjured(item)
+                    self.__handle_conjured(item)
                 
-                elif item.quality != 0:
-                    item.quality -= 1
-                    if item.sell_in < 0 and item.quality > 0:
-                        item.quality -= 1
-        
+                else:
+                    self.__handle_common_items(item)
+
+            if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in -= 1
+            else:
+                self.__handle_sulfuras()
+
+    def __handle_common_items(self, item):
+        item.quality -= 1
+
+        if item.sell_in < 0 and item.quality > 0:
+            item.quality -= 1
 
     def __handle_aged_brie(self, item):
-        item.quality += 1
+        if item.quality < 50: 
+            item.quality += 1
         
     def __handle_backstage(self, item):
         
@@ -46,8 +51,7 @@ class GildedRose(object):
         if item.sell_in < 0:
             item.quality = 0
 
-    def __handle_sulfuras(self, item):
-        item.sell_in = 0
+    def __handle_sulfuras(self):
         return
         
     def __handle_conjured(self, item):
@@ -56,8 +60,10 @@ class GildedRose(object):
         if item.quality > 0:
             item.quality -= 1
 
-        if item.sell_in < 0 and item.quality - 1 > 0:
-            item.quality -= 2
+        if item.sell_in < 0:
+            item.quality -= 1
+            if item.quality > 0:
+                item.quality -= 1
 
 class Item:
     def __init__(self, name, sell_in, quality):
